9ae1766926184377c36812bec1e9cb27f63df9e7
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2021-11-23 17:02:21 -0800

    Update documentation for the Vertex class:

--- include/vertex.h
+++ include/vertex.h
@@ -3,63 +3,90 @@
 #include <vector>
 #include "edge.h"
 
+/**
+ * Predeclare the Edge class to resolve the circular dependency issue 
+ * that arises out of our adjacency list graph implementation. The compiler 
+ * needs to know the size of each type contained as members of the Vertex class. 
+ * We are only using Edge*, so the exact size of Edge does not matter. This 
+ * means we can predeclare the Edge class in the Vertex class header file and 
+ * finish the implementation of the Edge class in its own respective header and 
+ * implementation files. 
+ */
 class Edge;
 
+/**
+ * @brief This class represents an individual involed in a transaction. An 
+ * individual is represented solely by their address on the blockchain and by 
+ * the transactions they are involved in. Refer to the Edge class for more 
+ * detail on how transactions are represented. 
+ */
 class Vertex {
   public:
-    Vertex(const std::string& address) 
-      : address_(address), was_explored_(false) {}
+    /**
+     * @brief Construct a new Vertex object. By default Vertices are initialized 
+     * with an empty list of indicent edges. 
+     * 
+     * @param address a string indicating the address of the individual that 
+     * this Vertex represents. 
+     */
+    Vertex(const std::string& address);
 
+    /**
+     * @brief 
+     * 
+     * @param e an Edge* 
+     */
     void addEdge(Edge* e);
 
+    /**
+     * @brief 
+     * 
+     * @return const std::vector<Edge*>& 
+     */
     const std::vector<Edge*>& getIncidentEdges() const;
 
+    /**
+     * @brief 
+     * 
+     * @return const std::string& 
+     */
     const std::string& getAddress() const;
 
-    bool wasExplored() const { return was_explored_; }
+    /**
+     * @brief Get whether or not this Vertex has been explored in some iteration 
+     * of a graph algorithm. 
+     * 
+     * @return true if this Vertex HAS been explored in the current iteration of 
+     * some graph algorithm.
+     * @return false if this Vertex has NOT yet been explored in the current 
+     * iteration of some graph algorithm.
+     */
+    bool wasExplored() const;
 
-    void setExplored(bool explored) { was_explored_ = explored; }
+    /**
+     * @brief Set the whether or not this Vertex has been explored in some 
+     * iteration of a graph algorithm. 
+     * 
+     * @param explored a bool indicating the new exploration state of this 
+     * Vertex.
+     */
+    void setExplored(bool explored);
   private: 
+    /**
+     * @brief 
+     * 
+     */
     std::string address_;
+
+    /**
+     * @brief 
+     * 
+     */
     std::vector<Edge*> incident_edges_;
 
+    /**
+     * @brief a bool state used by graph algorithms to track whether or not this 
+     * Vertex has been explored in the current iteration of the algorithm. 
+     */
     bool was_explored_;
 };
-
-// std::stoll
-
-/*
-#include<iostream>
-#include<vector>
-#include<sstream>
-using namespace std;
-main() {
-   string my_str = "ABC,XYZ,Hello,World,25,C++";
-   vector<string> result;
-   stringstream s_stream(my_str); //create string stream from the string
-   while(s_stream.good()) {
-      string substr;
-      getline(s_stream, substr, ','); //get first string delimited by comma
-      result.push_back(substr);
-   }
-   for(int i = 0; i<result.size(); i++) {    //print all splitted strings
-      cout << result.at(i) << endl;
-   }
-}
-*/
-
-/*
-unordered_map<string, size_t> counts;
-
-	string line;
-    ifstream infile (filename);
-
-    if (infile.is_open()) {
-        while (getline(infile, line)) {
-            if (counts.find(line) == counts.end()) counts.insert({line, 1});
-            else counts[line] += 1;
-        }
-    }
-
-    infile.close();
-*/
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -1,5 +1,8 @@
 #include "../include/vertex.h"
 
+Vertex::Vertex(const std::string& address) 
+  : address_(address), was_explored_(false) {}
+
 void Vertex::addEdge(Edge* e) {
   incident_edges_.push_back(e);
 }
@@ -12,3 +15,10 @@ const std::string& Vertex::getAddress() const {
   return address_;
 }
 
+bool Vertex::wasExplored() const { 
+  return was_explored_; 
+}
+
+void Vertex::setExplored(bool explored) { 
+  was_explored_ = explored; 
+}
