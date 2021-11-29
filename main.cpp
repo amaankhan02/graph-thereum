e3646dad8c6d9666c979803f54492468b2f4d97e
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2021-11-29 08:18:09 -0600

    Update ArgumentParser interface to explicitly allow int, bool, string, and double CLI variables

--- main.cpp
+++ main.cpp
@@ -14,14 +14,10 @@ int main(int argc, char* argv[]) {
 
   ArgumentParser ap;
 
-  ap.add_argument("-f", ArgumentParser::DataType::STRING, true, &filepath, 
-                  "The path to the dataset to load as a graph.");
-  ap.add_argument("-b", ArgumentParser::DataType::BOOL, false, &should_run_bfs,
-                  "Indicates whether or not to run BFS on the graph.");
-  ap.add_argument("-d", ArgumentParser::DataType::BOOL, false, &should_run_djikstra,
-                  "Indicates whether or not to run Djikstra's on the graph.");
-  ap.add_argument("-c", ArgumentParser::DataType::BOOL, false, &should_run_betweeness,
-                  "Indicates whether or not to run betweeness centrality algorithms on the graph.");
+  ap.add_argument("-f", true, &filepath, "The path to the dataset to load as a graph.");
+  ap.add_argument("-b", false, &should_run_bfs, "Indicates whether or not to run BFS on the graph.");
+  ap.add_argument("-d", false, &should_run_djikstra, "Indicates whether or not to run Djikstra's on the graph.");
+  ap.add_argument("-c", false, &should_run_betweeness, "Indicates whether or not to run betweeness centrality algorithms on the graph.");
 
   ap.parse(argc, argv);
 
