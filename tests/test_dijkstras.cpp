83f8d2feef5c6d1e4001c9617cf06bdff30850f9
akulgupta30 <Akulg3@illinois.edu>
2021-12-02 17:03:59 -0600

    Added dijkstras test file

---
+++ tests/test_dijkstras.cpp
@@ -0,0 +1,17 @@
+#include "../cs225/catch/catch.hpp"
+#include "../include/vertex.h"
+#include "../include/graph.h"
+#include "../include/edge.h"
+#include "../include/bfs.h"
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::pair;
+
+TEST_CASE("Simple dijkstras on Graph with 1 Connected Component and 5 nodes", "[dijkstras]")
+{
+  Graph g;
+}
+
