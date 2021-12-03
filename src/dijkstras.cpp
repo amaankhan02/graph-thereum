9d0ea8d0b1d066faf567c8d5636d0c2b400c3652
amantk2 <amantk2@illinois.edu>
2021-12-02 21:41:22 -0600

    commit git

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,12 +1,19 @@
 #include "../include/dijkstras.h"
 #include "../include/edge.h"
 #include <queue>
+#include <iostream>
 
 uint64_t dijkstra(Graph* g, Vertex* start, Vertex* end) {
   std::priority_queue<Vertex*> q;
 
   // set everything to infinity
+  std::cout << "Printing vertices in order now" << std::endl;
+
   for (auto vertex : g->getVertices()) {
+
+    std::cout << vertex.second->getAddress() << std::endl;
+
+    // vertex.second is pointer to the vertex
     vertex.second->setDistance(0xFFFFFFFFFFFFFFFF); 
 
     // record that we have not yet visited this vertex
@@ -27,9 +34,9 @@ uint64_t dijkstra(Graph* g, Vertex* start, Vertex* end) {
     for (auto incicident_edge : U->getIncidentEdges()) {
       if (!incicident_edge->getAdjacentVertex(U)->wasExplored()) {
         if (U->getDistance() == 0xFFFFFFFFFFFFFFFF) {
-          temp_dist = incicident_edge->getGas();
+          temp_dist = incicident_edge->getGasPrice();
         } else {
-          temp_dist = U->getDistance() + incicident_edge->getGas();
+          temp_dist = U->getDistance() + incicident_edge->getGasPrice();
         }
 
         if (temp_dist < incicident_edge->getAdjacentVertex(U)->getDistance()) {
@@ -45,13 +52,22 @@ uint64_t dijkstra(Graph* g, Vertex* start, Vertex* end) {
 }
 
 uint64_t dijkstra(Graph* g) {
+    // std::cout << "Entered dijkstra helper function " << std::endl;
+
     const std::unordered_map<std::string, Vertex*>& temp = g->getVertices();
     auto it = temp.begin();
+
+    // std::cout << "About to identify start vertex " << std::endl;
     Vertex* start = it->second;
+
+    std::cout << "Start vertex is " << start->getAddress() << std::endl;
     
-    for (int i = 0; i < 5; ++i) ++it;
+    for (int i = 0; i < 1; ++i) ++it;
 
     Vertex* end = it->second;
+    std::cout << "End vertex is " << end->getAddress() << std::endl;
+
+    // std::cout << "Segfault even before calling dijkstra helper function " << std::endl;
 
     return dijkstra(g, start, end);
 };
