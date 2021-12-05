9efd5bf0bf30c426a0b125361fcfa93ffca90b7d
Akul Gupta <akulg3@siebl-0224-31.ews.illinois.edu>
2021-12-04 18:24:07 -0600

    Adjusted dijkstras tests

--- tests/test_dijkstras.cpp
+++ tests/test_dijkstras.cpp
@@ -7,9 +7,11 @@
 
 #include <iostream>
 #include <string>
+#include <list>
 
 using std::string;
 using std::pair;
+using std::list;
 
 TEST_CASE("Simple dijkstras distance is accurate on Graph with 1 Connected Component and 8 nodes", "[dijkstras]") {
   Graph g;
@@ -36,6 +38,43 @@ TEST_CASE("Simple dijkstras distance is accurate on Graph with 1 Connected Compo
 
   uint64_t distance = dijkstra(&g, v1, v8);
   REQUIRE(distance == 4);
+}
+
+TEST_CASE("Simple dijkstras path is accurate on Graph with 1 Connected Component and 8 nodes", "[dijkstras]") {
+  Graph g;
+
+  Vertex* v1 = g.addVertex("0x1");
+  Vertex* v2 = g.addVertex("0x2");
+  Vertex* v3 = g.addVertex("0x3");
+  Vertex* v4 = g.addVertex("0x4");
+  Vertex* v5 = g.addVertex("0x5");
+  Vertex* v6 = g.addVertex("0x6");
+  Vertex* v7 = g.addVertex("0x7");
+  Vertex* v8 = g.addVertex("0x8");
+
+  g.addEdge(v1, v2, 50, 1, 1);
+  g.addEdge(v1, v3, 21, 1, 1);
+  g.addEdge(v2, v4, 42, 1, 1);
+  g.addEdge(v3, v5, 57, 1, 1);
+  g.addEdge(v4, v5, 53, 1, 1);
+  g.addEdge(v4, v6, 57, 1, 1);
+  g.addEdge(v4, v7, 40, 1, 1);
+  g.addEdge(v5, v7, 63, 1, 1);
+  g.addEdge(v6, v7, 51, 1, 1);
+  g.addEdge(v7, v8, 1, 1, 1);
+
+  uint64_t distance = dijkstra(&g, v1, v8);
+
+  list<Vertex*> correct_path = list<Vertex*> {v8, v7, v4, v2, v1};
+  list<Vertex*> output_path = list<Vertex*> {};
+
+  Vertex* current = v8;
+  while(current != NULL) {
+    output_path.push_back(current);
+    current = v8->getParent();
+  }
+
+  REQUIRE(correct_path == output_path);
 }
 
 
